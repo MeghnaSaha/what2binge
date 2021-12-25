a9554a682b55e5ade6f72a394a529bf7d4baeb82
MeghnaSaha <meghnacs11@gmail.com>
2021-12-26 00:32:00 +0530

    Search feature POC

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,11 +4,27 @@ import { httpGet } from '../utils/rest';
 import ItemSummary from './ItemSummary';
 import ResultMessage from './ResultMessage';
 import Search from './Search';
+import searchIcon from '../assets/search-50.png';
 
 export default function Gallery() {
 
     const [searchResultsReady, setSearchResultsReady] = useState(false);
     const [searchResults, setSearchResults] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const searchData = (e) => {
+        e.preventDefault();
+        setSearchResultsReady(false);
+        console.log(searchQuery);
+        httpGet(MOVIE_SEARCH(searchQuery))
+        .then((response) => {
+            if(response.data.Response === 'True')
+                setSearchResults(response.data.Search);
+            setSearchResultsReady(true);
+        }, (error) => {
+            console.log(error);
+        });
+    }
 
     useEffect(() => {
         httpGet(MOVIE_SEARCH('pizza'))
@@ -24,7 +40,14 @@ export default function Gallery() {
 
     return (
         <div className="container">
-            <Search/>
+            <div className="search-area">
+                <form onSubmit={searchData}>
+                    <input className="search-box" type="text" value={searchQuery} onChange={e=>setSearchQuery(e.target.value)} placeholder="What are we feeling like?"/>
+                    <button className="search-btn">
+                        <img className='searchIcon' src={searchIcon} alt='search' />
+                    </button>
+                </form>
+            </div>
             {searchResultsReady ?
                 (searchResults.length === 0 ? 
                     <ResultMessage messageTitle='You have an atypical taste!' message='No matches found in our collection - please try a different query.'/>
